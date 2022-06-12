0a766f8cf5581faef3f84e20e74f0dc024efb831
yixiaozhang <yixiao.zhang@mail.utoronto.ca>
2022-06-13 00:46:23 +0800

    update

--- phase0/Work_for_phase0/src/main.java
+++ phase0/Work_for_phase0/src/main.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
 public class main {
+    public static void main(String[] args) {
+        System.out.println("What do you want to do Today? Sign in?(type A) enter: ");
+        Scanner sc = new Scanner(System.in);
+        String task = sc.next();
+        System.out.println(task);
+    }
 }

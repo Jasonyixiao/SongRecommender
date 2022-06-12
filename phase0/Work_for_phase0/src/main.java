29b38b17aa6731bb9a89207860b13254583ce0ec
yixiaozhang <yixiao.zhang@mail.utoronto.ca>
2022-06-13 00:52:39 +0800

    update

--- phase0/Work_for_phase0/src/main.java
+++ phase0/Work_for_phase0/src/main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        System.out.println("What do you want to do Today? Sign in?(type A) enter: ");
+        System.out.println("What do you want to do Today? input A if you want to promote, input B if delete. " +
+                "your input: ");
         Scanner sc = new Scanner(System.in);
         String task = sc.next();
         System.out.println(task);

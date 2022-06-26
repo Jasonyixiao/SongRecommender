94c9e043a987586cc489dad033cbd002ee3a4748
Selenarn <selenarn.liu@mail.utoronto.ca>
2022-06-26 19:53:11 -0400

    part of the test

--- phase0/Work_for_phase0/test/test.java
+++ phase0/Work_for_phase0/test/test.java
@@ -1,2 +1,43 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import entities.User;
+import java.util.ArrayList;
+
 public class test {
+    @Before
+    public void setUp() {
+    }
+    @After
+    public void tearDown() {
+    }
+
+    // test constructor
+    @Test(timeout=50)
+    public void () {
+    }
+    @Test(timeout = 50)
+    public void () {
+    }
+    @Test(timeout = 50)
+    public void () {
+    }
+    @Test(timeout=50)
+    public void () {
+    }
+    @Test(timeout = 50)
+    public void () {
+    }
+    @Test(timeout = 50)
+    public void () {
+    }
+    @Test(timeout=50)
+    public void () {
+    }
+    @Test(timeout = 50)
+    public void () {
+    }
+    @Test(timeout = 50)
+    public void () {
+    }
+
 }

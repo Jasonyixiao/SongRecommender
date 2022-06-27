79c9e2c1fca98e3a9061faa875ff57a5cf275f40
yixiaozhang <yixiao.zhang@mail.utoronto.ca>
2022-06-27 17:27:34 +0800

    Update, fixed some bugs.

--- phase0/TopNine/src/TopNine.java
+++
@@ -1,5 +0,0 @@
-public class TopNine {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
--- phase0/TopNine/src/controllers/ILoginController.java
+++
@@ -1,9 +0,0 @@
-package controllers;
-
-public interface ILoginController {
-
-    // Userprofile should be created here.
-    UserProfile Login(String userName, String password);
-    void LogOff(UserProfile userProfile);
-
-}

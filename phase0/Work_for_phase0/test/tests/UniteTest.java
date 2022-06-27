109dd1c5beafa5059e25dea9d7d28d46c1342bd3
Selenarn <selenarn.liu@mail.utoronto.ca>
2022-06-26 23:50:06 -0400

    part of the test

--- phase0/Work_for_phase0/test/tests/UniteTest.java
+++ phase0/Work_for_phase0/test/tests/UniteTest.java
@@ -1,21 +1,43 @@
 package tests;
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import commands.Commands;
+import controllers.ShellState;
+import driver.LoginSystem;
+import commands.CommandController;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class UniteTest {
+    LoginSystem loginSystem;
+    CommandController commandExecutor;
+    ShellState shellstate;
+
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        loginSystem = new LoginSystem();
+        commandExecutor = new CommandController(shellstate);
     }
     @After
-    public void tearDown() {
+    public void tearDown() throws Exception {
     }
 
     // test constructor
-    @Test(timeout=50)
-    public void () {
+    @Test
+    public void testgetIsSignedIn() {
+        try {
+            HashMap<String, Commands> output = commandExecutor.getCommandMap();
+            boolean getissignedin = shellstate.getIsSignedIn();
+            assertEquals("T", getissignedin);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }

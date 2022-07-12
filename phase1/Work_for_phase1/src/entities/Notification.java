8fc4695b2fe9212ddce686e795c267c9df0faf6f
yixiaozhang <yixiao.zhang@mail.utoronto.ca>
2022-07-13 00:01:00 +0800

    added new class

---
+++ phase1/Work_for_phase1/src/entities/Notification.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class Notification {
+    private String message; // possibly other user's message
+    private String Content; // for now it is an url
+    public Notification(String message ) {
+        this.message =message;
+
+    }
+}

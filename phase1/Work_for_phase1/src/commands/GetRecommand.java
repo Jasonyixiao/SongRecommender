0b5df1ebb79e091296c8c1e74d577bbebaaec920
weiheng wang <weiheng.wang@mail.utoronto.ca>
2022-07-19 03:38:44 +0800

    aaaaa

---
+++ phase1/Work_for_phase1/src/commands/GetRecommand.java
@@ -0,0 +1,12 @@
+package commands;
+
+import controllers.ShellState;
+
+import java.io.IOException;
+
+public class GetRecommand implements Commands{
+    @Override
+    public String executeCommand(ShellState state) throws IOException {
+        return null;
+    }
+}
--- phase1/Work_for_phase1/src/usecases/SongManager.java
+++ phase1/Work_for_phase1/src/usecases/SongManager.java
@@ -2,7 +2,9 @@ package usecases;
 
 import entities.Song;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * This class will allow the system users to do stuff with a song.
@@ -35,5 +37,36 @@ public class SongManager {
 
     public float displayCurrentRating(String songName){
         return allSongs.get(songName).getAvgRating();
+    }
+
+    public List<String> getRecommand(int amount){
+        ArrayList<Song> result = new ArrayList<>();
+        for(Song s : allSongs.values()){
+            if(result.size()<amount){
+                result.add(s);
+            }else{
+                replaceSmallerRating(result,s);
+            }
+        }
+        ArrayList<String> trueResult = new ArrayList<>();
+        for(Song s: result){
+            trueResult.add(s.getName());
+        }
+        return trueResult;
+    }
+    private void replaceSmallerRating(List<Song> l, Song newSong){
+        Song s = l.get(0);
+        for (Song song: l) {
+            if(song.getAvgRating()<s.getAvgRating()){
+                s = song;
+            }
+        }
+        if(s.getAvgRating()< newSong.getAvgRating()){
+            l.remove(s);
+            l.add(newSong);
+        }
+    }
+    public String getURL(String name){
+        return allSongs.get(name).getSongUrl();
     }
 }

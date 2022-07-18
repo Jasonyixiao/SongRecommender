b433d19b0f005240a767f782433ed6bfd05eb2f5
Selenarn <selenarn.liu@mail.utoronto.ca>
2022-07-18 18:21:30 -0400

    part of the loginSystem, commandController and new commands file

---
+++ phase1/Work_for_phase1/src/commands/RatingSong.java
@@ -0,0 +1,18 @@
+package commands;
+
+import controllers.ShellState;
+import controllers.UserController;
+
+import java.util.Scanner;
+
+public class RatingSong implements Commands{
+
+    public RatingSong() {
+    }
+
+    public String executeCommand(ShellState state) {
+        try { ...
+        }
+    }
+
+}
---
+++ phase1/Work_for_phase1/src/commands/SearchSong.java
@@ -0,0 +1,14 @@
+package commands;
+
+import controllers.ShellState;
+import controllers.UserController;
+
+import java.util.Scanner;
+
+public class SearchSong implements Commands{
+
+    public SearchSong() {
+        try { ...
+        }
+    }
+}

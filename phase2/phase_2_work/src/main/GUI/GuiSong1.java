b2e4dcacc1e33d34b76f69d152e87f5e39b56d56
weiheng wang <weiheng.wang@mail.utoronto.ca>
2022-08-04 07:59:42 +0800

    Notification parts

--- phase2/phase_2_work/src/main/GUI/GuiSong1.java
+++ phase2/phase_2_work/src/main/GUI/GuiSong1.java
@@ -16,11 +16,11 @@ public class GuiSong1{
 
 
         JLabel rate = new JLabel("Rate（Use 1-5 any characters present stars) ");
-        rate.setBounds(20,20,150,25);
+        rate.setBounds(20,20,300,25);
         panel.add(rate);
 
         JTextField rateField = new JTextField(5);
-        rateField.setBounds(50,20,45,25);
+        rateField.setBounds(350,20,45,25);
         panel.add(rateField);
         frame.setVisible(true);
 

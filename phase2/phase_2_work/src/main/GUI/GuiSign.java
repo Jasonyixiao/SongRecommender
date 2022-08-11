8b7f684f5a1dac273652cce71f065ce376474a10
yixiaozhang <yixiao.zhang@mail.utoronto.ca>
2022-08-11 18:14:13 +0800

    added more back buttons and made the panels scrollable

--- phase2/phase_2_work/src/main/GUI/GuiSign.java
+++ phase2/phase_2_work/src/main/GUI/GuiSign.java
@@ -25,20 +25,36 @@ class GuiSign extends JDialog {
 
         JPanel panel1 = new JPanel();
         JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
 
-        panel1.setBorder(BorderFactory.createEmptyBorder(200, 150, 200, 250));
-        panel2.setBorder(BorderFactory.createEmptyBorder(200, 250, 200, 150));
+        panel1.setBorder(BorderFactory.createEmptyBorder(200, 100, 200, 250));
+        panel2.setBorder(BorderFactory.createEmptyBorder(200, 250, 200, 100));
+        panel3.setBorder(BorderFactory.createEmptyBorder(250, 150, 100, 150));
 
         panel1.setLayout(new GridLayout(3, 3));
         panel2.setLayout(new GridLayout(3, 3));
 
-        panel1.add(button1); // exsit user
+        panel1.add(button1); // exit user
         panel1.add(label1);
         panel2.add(button2);//new user
         panel2.add(label2);
         frame.add(panel1, BorderLayout.LINE_START);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.add(panel2, BorderLayout.LINE_END);
+        frame.add(panel3);
+
+        //Back button(return to main page:
+        JButton backButton = new JButton(languageGetter.translateTo(language).back());
+        button2.setBounds(400,500,80,25);
+        panel3.add(backButton);
+        //Add actionlistener to button2
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new GuiChooseLanguage(shell);
+                frame.dispose();
+            }
+        });
 
         button1.addActionListener(new ActionListener() {
             @Override
